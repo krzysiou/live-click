8663083f60dbc551215572e8298e3d80574f65af
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2021-04-02 18:32:05 +0200

    set up tailwind

---
+++ src/components/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1 class="text-lg font-semibold">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
