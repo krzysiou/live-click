7780ac49024d8af2f25f8bd2aaba17df3d1d0e9c
Krzysiek Tłuszcz <krzysztof.kt.tluszcz@gmail.com>
2021-07-19 16:33:03 +0200

    change layout

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,30 +1,46 @@
 <template>
-    <div class="flex items-center justify-center w-full h-full py-48 min-h-screen bg-gray-100">
-    <div class="relative py-8 w-80">
-        <div class="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 shadow-lg transform scale-95 -skew-x-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
-        <div class="relative px-4 py-10 bg-white shadow-lg rounded-3xl sm:p-5 flex flex-col justify-around">
-            <form>
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                        Username
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    <div class="flex flex-col justify-around items-center w-full h-full min-h-screen bg-gray-100">
+        <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div class="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 shadow-lg transform -skew-x-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
+        <div class="transition duration-700 transform hover:scale-105 relative px-4 py-10 bg-white shadow-lg rounded-3xl sm:p-20">
+            <div class="max-w-md mx-auto">
+            <div class="divide-y divide-gray-200">
+                <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <p class="font-bold">Welcome to Live Click</p>
+                <p>Create your own room and invite guests, let them compete on whoever pushes the button first.</p>
                 </div>
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                        Password
-                    </label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password">
+                <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+                <p>Click the icon below to create your own room.</p>
                 </div>
-                <p v-if="error" class="text-red-500 mb-4">{{error}}</p>
-                <p class="mb-4 text-sm">Dont have an account? Register <a href="http://localhost:8080/#/register" class="text-blue-500">here</a></p> 
-                <div class="flex items-center justify-around">
-                    <button @click="submit()" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                        Sign In
-                    </button>
-                </div>
-            </form>
+            </div>
+            </div>
         </div>
+        </div>  
+        <div class="relative py-8 w-80">
+            <div class="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 shadow-lg transform scale-95 -skew-x-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
+            <div class="relative px-4 py-10 bg-white shadow-lg rounded-3xl sm:p-5 flex flex-col justify-around">
+                <form>
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                            Username
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                    </div>
+                    <div class="mb-6">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                            Password
+                        </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password">
+                    </div>
+                    <p v-if="error" class="text-red-500 mb-4">{{error}}</p>
+                    <p class="mb-4 text-sm">Dont have an account? Register <a href="http://localhost:8080/#/register" class="text-blue-500">here</a></p> 
+                    <div class="flex items-center justify-around">
+                        <button @click="submit()" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                            Sign In
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </template>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,27 +1,10 @@
 <template>
-  <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-around sm:py-12">
-    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-      <div class="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 shadow-lg transform -skew-x-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
-      <div class="transition duration-700 transform hover:scale-105 relative px-4 py-10 bg-white shadow-lg rounded-3xl sm:p-20">
-        <div class="max-w-md mx-auto">
-          <div class="divide-y divide-gray-200">
-            <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-              <p class="font-bold">Welcome to Live Click</p>
-              <p>Create your own room and invite guests, let them compete on whoever pushes the button first.</p>
-            </div>
-            <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
-              <p>Click the icon below to create your own room.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+  <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+    <div class="relative py-3 mb-40 sm:max-w-xl sm:mx-auto">
       <div class="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 shadow-lg transform scale-95 -skew-x-6 sm:skew-y-0 sm:-rotate-6 rounded-3xl"></div>
       <div class="relative px-4 py-10 bg-white shadow-lg rounded-3xl sm:p-5 flex flex-col justify-around">
         <p class="font-bold">Set custom name</p>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="newname" type="text" placeholder="Name">
+        <input class="shadow appearance-none border rounded w-full mt-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="newname" type="text" placeholder="Name">
         <p v-if="error" class="text-red-500 mt-4">{{error}}</p>
         <div class="flex items-center justify-center">
           <button @click="submit()" class="bg-green-500 hover:bg-green-600 text-white font-bold my-4 w-24 py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
